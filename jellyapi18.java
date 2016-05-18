f9327ad136c2a45bc824341706a841077d8909dd
PixlieCD <kjssdfff@gmail.com>
2016-05-19 00:38:24 +0430

    Create jellyapi18.java

---
+++ jellyapi18.java
@@ -0,0 +1,44 @@
+public class jellyapi18 {
+
+ public static void main(String[] args)
+ {
+  System.out.println("Server will start in 10 seconds");
+  System.out.println("You are using JellyAPI for your Server. more at: http://www.helpkins.eu/jellyapi18");
+  try{
+     Thread.sleep(10000);
+}catch(InterruptedException e){}
+  System.out.println("Server started generating Files: 'config.properties, Server.properties, JellyAPI.yml, World.dir, nether_World.dir, end_World.dir, Plugins.dir, Mods.dir, Librarys.jel, EULA.txt' ...");
+  try{
+  Thread.sleep(2500);
+  }catch(InterruptedException e){}
+  System.out.println("FILES CREATES, SERVER STARTED AT PORT/IP: 0.0.0.0:25565");
+  System.out.println(">[CONSOLE] type /help for commands.");
+  System.out.println(">");
+  try{
+  Thread.sleep(2500);
+  }catch(InterruptedException e){}
+  System.out.println(">help");
+  System.out.println(">[CONSOLE-INFO] help Command is Only for Player!");
+ try{
+ Thread.sleep(5000);
+ }catch(InterruptedException e) {}
+ System.out.println(">[CONSOLE-INFO]Player Test(192.81.34.56) has joined Server(0.0.0.0)");
+ System.out.println(">[INFO] Test has Joined the Game");
+ try{
+ Thread.sleep(5000);
+ }catch(InterruptedException e){}
+ System.out.println(">[CONSOLE-INFO]Player Test(192.81.34.56) has leaved Server(0.0.0.0)");
+ System.out.println(">[INFO] Test has Leaved the Game");
+  try{
+  Thread.sleep(10000);
+  }catch(InterruptedException e){}
+  System.out.println(">[SERVER-INFO] Server is Shutting down with 0253/1024M and 0 Players at Ip and Port 0.0.0.0:00000.");
+  System.out.println(">[SERVER-INFO] Server Shutted Down Succesfully.");
+  System.out.println("Thank you for using JellyAPI as your Server software!");
+  try{
+  Thread.sleep(200);
+  }catch(InterruptedException e) {}
+  System.out.println("C:/User/root/_ ");
+ }
+
+}
